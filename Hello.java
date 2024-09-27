2c7184996055d37a84799c5de0294362aa60b731
Alemtuba Longkumer <alem@wifi-wpa-cw-140-193-208-52.wifi.umanitoba.ca>
2024-09-27 17:47:30 -0500

    Added Hello.java and .Rhistory to git repo

---
+++ Hello.java
@@ -0,0 +1,28 @@
+# Coding-Kickoff-2024
+
+## Welcome to Coding Kickoff 2024!
+
+This is the git portion of the workshop.
+
+Here are the basic commands that you will be introduced to:
+
+    - git init 
+    - git clone
+    - git add 
+    - git commit 
+    - git push
+    - git branch 
+    - git checkout 
+    - git pull
+    - git merge 
+    - git stash
+    - git status 
+    - git log
+    - git revert
+    - git reset
+
+Other useful git things to know:
+
+### Good commit message practices
+### Good git workflow
+### Pull Request/Merge Request
